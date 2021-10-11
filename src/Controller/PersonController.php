75181bb0a2f8ddfecf058a2db79260ed60f8dd93
julierebeyrolle <julie.rebeyrolle@gmail.com>
2021-10-11 17:44:39 +0200

    Create Person Controller

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/person", name="person_")
+ */
+class PersonController extends AbstractController
+{
+    /**
+     * @Route ("/", methods={"GET"}, name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PersonController.php',
+        ]);
+    }
+}
