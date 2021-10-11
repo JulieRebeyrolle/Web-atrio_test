90bdf6b953af48c0ad8e8b8bc7f6315d15328342
julierebeyrolle <julie.rebeyrolle@gmail.com>
2021-10-11 18:00:05 +0200

    Create Person new method

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/person", name="person_")
@@ -22,4 +29,22 @@ class PersonController extends AbstractController
             'path' => 'src/Controller/PersonController.php',
         ]);
     }
+
+    /**
+     * @Route ("/", methods={"POST"}, name="new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $person = $this->get('serializer')->deserialize($request->getContent(), Person::class, 'json');
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($person);
+
+        $entityManager->flush();
+
+        return $this->json(["success" => true, "data" => $person], 201);
+    }
+
+
 }
